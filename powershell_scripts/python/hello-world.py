7de79f622aeb1a2be3ab1e8777d175f7ce6f14d0
Piotr Surowka <piotr.surowka90.dev@gmail.com>
2020-04-21 05:12:15 -0700

    Added python to powershell script

---
+++ powershell_scripts/python/hello-world.py
@@ -0,0 +1,11 @@
+# Python code to demonstrate 
+# working of sleep() 
+  
+import time 
+  
+# printing the start time  
+print("Hello world from Python!") 
+print(time.ctime()) 
+  
+# using sleep() to hault the code execution 
+time.sleep(2) 
