83e6c078da4b580c4f36ac69fa3a0e4eb74c3906
Piotr Surowka <piotr.surowka90.dev@gmail.com>
2020-04-21 06:41:10 -0700

    Antoher small change

--- MyGet.ps1
+++ MyGet.ps1
@@ -1,3 +1,1 @@
-﻿Write-Host "Congratulations! Your first script executed successfully"
-Start-Process -FilePath python.exe -ArgumentList 'hello-world.py' 
-Start-Sleep -s 15
+﻿Start-Process -FilePath python.exe -ArgumentList 'hello-world.py' 
